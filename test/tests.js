022dc2b1a94cf20f3ae471e6c4b3e22937ce006b
Louskil <tbikulov@gmail.com>
2025-09-25 03:08:20 +0300

    RED#1_First_test

---
+++ test/tests.js
@@ -0,0 +1,14 @@
+import { expect } from 'chai';
+//import { Matrix } from '../src/matrix.js';
+
+describe('Matrix Library - TDD Approach', function() {
+    
+    describe('Matrix Creation', function() {
+        it('should create empty 2x2 matrix with zeros', function() {
+            const matrix = new Matrix(2, 2);
+            expect(matrix.rows).to.equal(2);
+            expect(matrix.cols).to.equal(2);
+            expect(matrix.get(0, 0)).to.equal(0);
+        });
+    });
+});
